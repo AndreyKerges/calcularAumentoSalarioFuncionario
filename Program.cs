76999f923febf56f381f1d2aa966690436a0571f
Andrey Kerges Nascimento <andrey.nascimento2@fatec.sp.gov.br>
2024-09-03 21:14:32 -0300

    Exercício Finalizado

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿double salario;
+string nome;
+
+Console.WriteLine("--- Aumento Salário ---");
+
+Console.WriteLine("Digite seu nome: ");
+nome = Console.ReadLine()!;
+
+Console.WriteLine($"Olá {nome}, Digite seu salário: ");
+salario = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Tudo bem {nome}, seu salário é: {salario} ");
+
+if (salario <= 500)
+{
+    salario  = salario * 1.20;
+    Console.WriteLine($"Com o aumento, seu salário ficou: {salario}");
+}
+else
+{
+    salario = salario * 1.10;
+    Console.WriteLine($"Com o aumento, seu salário ficou: {salario}");
+}
+
